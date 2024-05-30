4af8e0b8a6c756333ff74ff6cb85d77ed26fb74a
mdmatvey <lyoshinmdmatvey@gmail.com>
2024-05-30 13:40:16 +0300

    feat: switched OLED display to LCD screen

--- climateSensor/climateSensor.ino
+++ climateSensor/climateSensor.ino
@@ -1,54 +1,42 @@
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include "DHT.h"
+#include <DHT.h> 
+#include <LiquidCrystal_I2C.h>
 
 #define DHTPIN 2 
-#define DHTTYPE DHT11 // DHT 11
+#define DHTTYPE DHT22
 
-// software SPI:
-#define SCREEN_WIDTH 128 
-#define SCREEN_HEIGHT 64
+#define I2C_ADDR 0x27 
+#define BACKLIGHT_PIN 3
+#define En_pin 2
+#define Rw_pin 1
+#define Rs_pin 0
+#define D4_pin 4
+#define D5_pin 5
+#define D6_pin 6
+#define D7_pin 7
 
-#define OLED_MOSI  11
-#define OLED_CLK   13
-#define OLED_DC     9
-#define OLED_CS    10
-#define OLED_RESET  8
-#define maxString 21
-char target[maxString + 1] = "";
-unsigned char i1,i2,c3;
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+LiquidCrystal_I2C lcd(0x27,20,4);
 DHT dht(DHTPIN, DHTTYPE);
-void setup(){           
-  display.begin(SSD1306_SWITCHCAPVCC);
-  display.cp437(true);
+
+void setup(){
+  lcd.init();
+  lcd.init();
+  lcd.backlight();
   dht.begin();
   Serial.begin(57600);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Hudim.:");
+  lcd.setCursor(0, 1);
+  lcd.print("Temp.:");
 }
-void loop() {
+
+void loop()
+{
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-  // float hic = dht.computeHeatIndex(t, h, false);
-  display.clearDisplay(); 
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.setCursor(0,0);
-  display.println(utf8rus("Температура: "));
-  display.setTextSize(2);
-  display.setCursor(10,10);
-  display.println(t);
-  display.setCursor(0,30);
-  display.setTextSize(1);
-  display.println(utf8rus("Влажность: "));
-  display.setTextSize(2);
-  display.setCursor(10,40);
-  display.println(h);
-  display.setCursor(75,10);
-  display.println("\xB0");
-  display.setCursor(75,40);
-  display.println("%");
-  display.display(); 
- 
+
+  lcd.setCursor(11, 0);
+  lcd.print(h);
+  lcd.setCursor(11, 1);
+  lcd.print(t);
 }
